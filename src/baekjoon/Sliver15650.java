bdb67eb474746f49d74a68b9cd97f05a1f8b0933
jongyeob <230233@naver.com>
2024-12-19 19:17:14 +0900

    N과 M (2) 15650번
    등급: 실버 3
    풀이법: 백트래킹
     - 현재 담긴 배열의 최대값 보다 i가 높을 때만 실행
     - depth가 줄어들면 arr 배열의 해당 depth 인덱스 값을 0으로 초기화

---
+++ src/baekjoon/Sliver15650.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Sliver15650 {
+    static int n = 0;
+    static int m = 0;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        br.close();
+        arr = new int[m];
+        visited = new boolean[n+1];
+
+        dfs(0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    static void dfs(int node){
+        if(node == m){
+            for(int i : arr){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i<= n;i++){
+            if(!visited[i] && Arrays.stream(arr).max().getAsInt() < i){
+                arr[node] = i;
+                visited[i] = true;
+                dfs(node + 1);
+                visited[i] = false;
+            }
+        }
+        arr[node] = 0;
+    }
+}
