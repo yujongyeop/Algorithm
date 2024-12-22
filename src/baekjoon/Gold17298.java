6996d2200e577772b7fa1a60cb7ea6e676f2f7ff
jongyeob <230233@naver.com>
2024-12-22 17:16:58 +0900

    오큰수 17298번 - 코드 개선
    등급: 골드 4
    풀이법: 스택
     - 기존 2개의 스택에서 단일 스택 사용으로 변경
     - 스택에서는 인덱스값을 넣음
     - 0~N번 순으로 접근하며, peek 인덱스의 값이 i번째 값보다 작으면, 스택에서 pop 시키는 구조

--- src/baekjoon/Gold17298.java
+++ src/baekjoon/Gold17298.java
@@ -1,42 +1,32 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Gold17298 {
     static int N;
-    static Stack<Integer> inputStack = new Stack<>();
-    static Stack<Integer> comparisonStack = new Stack<>();
+    static Stack<Integer> idxStack = new Stack<>();
     static int[] result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         result = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
         br.close();
-        while(st.hasMoreTokens()){
-            inputStack.push(Integer.parseInt(st.nextToken()));
-        }
-        comparisonStack.push(inputStack.pop());
-        result[N-1] = -1;
-        while (!inputStack.isEmpty()) {
-            int pop = inputStack.pop();
-            while(!comparisonStack.isEmpty()){
-                if(pop < comparisonStack.peek()){
-                    result[inputStack.size()] = comparisonStack.peek();
-                    comparisonStack.push(pop);
-                    break;
-                }else{
-                    comparisonStack.pop();
-                }
+        for(int i = 0 ; i < N ; i++){
+            while(!idxStack.isEmpty() && input[idxStack.peek()] < input[i]){
+                result[idxStack.pop()] = input[i];
             }
-            if(comparisonStack.isEmpty()){
-                result[inputStack.size()] = -1;
-            }
-            comparisonStack.push(pop);
+            idxStack.push(i);
         }
+
+        while(!idxStack.isEmpty()){
+            result[idxStack.pop()] = -1;
+        }
+
         for(int i =0;i<N;i++){
             bw.write(result[i] + " ");
         }
